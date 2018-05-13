19389f7a9e78fa81fa82bb57bb95e293b64b559c
Farrukht <Ftulkunov@gmail.com>
2018-05-13 19:10:56 +0500

    Ex2

---
+++ calculate.js
@@ -0,0 +1,29 @@
+
+
+function calculate()
+{
+	var weight = document.getElementById("weight").value;
+	var height = document.getElementById("height").value;
+	var score = document.getElementById('score');
+	var type = document.getElementById("type");
+	var BMI = 703*weight/(height*height);
+
+	score.innerHTML = BMI;
+
+		if(BMI<18)
+	{
+		type.innerHTML = "Underweight";
+	}
+		else if(BMI<25)
+	{
+		type.innerHTML = "Normal";
+	}
+		else if(BMI<30)
+	{
+		type.innerHTML = "Overweight";
+	}
+		else
+	{
+		type.innerHTML = "Obese";
+	}
+}
